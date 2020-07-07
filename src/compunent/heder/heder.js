60a18edbba483481925a306437de5f695cf365d1
boaz katz <boazkatz89@gmail.com>
2020-07-07 13:23:52 +0300

    first commit

---
+++ src/compunent/heder/heder.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./heder.css";
+
+function Heder() {
+  return (
+    <div>
+      <div className="Heder">Heder</div>
+    </div>
+  );
+}
+
+export default Heder;
---
+++ src/compunent/product/pro.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./pro.css";
+
+function Product() {
+  return (
+    <div>
+      <div className="Product"> Product</div>
+    </div>
+  );
+}
+
+export default Product;
