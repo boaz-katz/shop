ce4a7c2e045fe56bf0a483aa8f5bcb9338a2f663
boaz katz <boazkatz89@gmail.com>
2020-07-07 14:30:24 +0300

    shop

--- src/App.js
+++ src/App.js
@@ -3,18 +3,26 @@ import "./App.css";
 import Prodact from "./compunent/product/pro";
 import Heder from "./compunent/heder/heder";
 
-function App() {
-  return (
-    <div className="App">
-      <Heder />
-      <Prodact />
-      <Prodact />
-      <Prodact />
-      <Prodact />
-      <Prodact />
-      <Prodact />
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <div className="App">
+          <Heder />
+          <Prodact
+            title="tomato"
+            amount={5}
+            imgis="https://www.zeraim.com/sites/g/files/zhg866/f/styles/syngenta_cta_teaser/public/tory_s_2.png?itok=LpJp9sAm"
+          />
+          <Prodact title="cucumber" amount={5} />
+          <Prodact title="tomato" amount={5} />
+          <Prodact title="tomato" amount={7} />
+          <Prodact title="tomato" amount={4} />
+          <Prodact title="tomato" amount={5} />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/compunent/product/pro.js
+++ src/compunent/product/pro.js
@@ -2,12 +2,27 @@ import React from "react";
 
 import "./pro.css";
 
-function Product() {
-  return (
-    <div>
-      <div className="Product"> Product</div>
-    </div>
-  );
-}
+class Product extends React.Component {
+  state = {
+    amount: this.props.amount,
+  };
+  incrementCount = () => {
+    this.setState(({ amount }) => ({ amount: amount - 1 }));
+  };
 
+  render() {
+    return (
+      <div>
+        <div className="Product">
+          <p>{this.props.title}</p>
+          <img className="imges" src={this.props.imgis} />
+          <div>Count: {this.state.amount}</div>
+          <div>
+            <button onClick={this.incrementCount}>Increment Count</button>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
 export default Product;
